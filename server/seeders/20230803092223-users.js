b7165f35c72116778e4a8052f9061b599154f155
irzaarivin <irzaarivin2005@gmail.com>
2023-08-03 16:30:01 +0700

    Nambahin seeder

---
+++ server/seeders/20230803092223-users.js
@@ -0,0 +1,27 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const password = await bcrypt.hash('admin', 10);
+    await queryInterface.bulkInsert('Users', [{
+      name: 'admin',
+      email: 'admin@gmail.com',
+      password: password,
+      role: 'teacher',
+      value: 0,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
