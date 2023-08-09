7e5f2f578dd02b29146ce60fa668854ca48c3ce3
Irza Arivin <irzaarivin2005@gmail.com>
2023-08-09 22:45:42 +0700

    Lupa Maszheh

--- server/app.js
+++ server/app.js
@@ -27,59 +27,11 @@ const ExtractJWT = require('passport-jwt').ExtractJwt;
 const secretKey = "sangat_very_secret_key_banget_sekali";
 
 // Sinkronisasi model dengan database
-db.sequelize.sync()
-  .then(() => {
-    console.log('Database synced');
-  })
-  .catch((err) => {
-    console.error('Error syncing database: ', err);
-  });
-
-// CONFIGURE PASSPORT
-app.use(passport.initialize());
-const jwtOptions = {
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: secretKey,
-};
-
-passport.use(
-  new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-  }, async (email, password, done) => {
-    try {
-      const user = await User.findOne({ where: { email } });
-      if (!user) {
-        return done(null, false, { message: 'Email tidak ditemukan.' });
-      }
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        return done(null, false, { message: 'Password salah.' });
-      }
-      return done(null, user);
-    } catch (error) {
-      return done(error);
-    }
-  })
-);
-
-passport.use(
-  new JwtStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: secretKey,
-  }, async (payload, done) => {
-    try {
-      const user = await User.findByPk(payload.id);
-      console.log(user);
-      if (!user) {
-        return done(null, false);
-      }
-      return done(null, user);
-    } catch (error) {
-      return done(error);
-    }
-  })
-);
+db.sequelize.sync().then(() => {
+  console.log('Database synced');
+}).catch((err) => {
+  console.error('Error syncing database: ', err);
+});
 
 // CONFIGURE REDIS
 // const redisClient = redis.createClient({
@@ -102,6 +54,7 @@ app.get('/users/email/:email', userController.getUserByEmail);
 app.delete('/users/delete/:id', passport.authenticate('jwt', { session: false }), userController.deleteUserById);
 app.delete('/users/delete/email/:email', passport.authenticate('jwt', { session: false }), userController.deleteUserByEmail);
 
+// RUNNING SERVER
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
